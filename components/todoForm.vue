5bcf8f4ed4bc12d00b4eb114548282abb46b7b88
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2021-08-02 23:02:51 +0900

    watchのテスト練習

--- components/todoForm.vue
+++ components/todoForm.vue
@@ -4,23 +4,33 @@
       v-model="newTodo",
       label="新しいTodo"
     )
+    v-checkbox(
+      v-model="checked",
+      :label="'純正'"
+    )
     v-btn.ml-3(
       dark=true,
       color="#33CCFF",
       @click="onSubmit"
-    ) 送信
+    ) 確認
 </template>
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { 
+  defineComponent,
+  ref,
+  watch,
+ } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup() {
-    const newTodo :string = ''
+    const newTodo = ref<string>('')
+    const checked = ref<boolean>(false)
     const onSubmit = () => {
-      console.log('まじ卍')
     }
+     
     return {
       newTodo,
+      checked,
       onSubmit
     }
   },
